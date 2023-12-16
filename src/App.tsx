8a009bf0c0ba4cb045e6f39108ed18cb1248167a
alen.jakob <grafuxa@gmail.com>
2023-12-16 23:27:50 +0100

    [WiP]: Work in Progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Link, Route, Routes, useSearchParams } from "react-router-dom";
 import { Home } from "./routes/Home";
 import { Contact } from "./routes/Contact";
+import { useEffect } from "react";
 
 const NAVIGATION_LINKS = [
 	{ id: 1, url: "/", name: "Home" },
@@ -10,10 +11,29 @@ const NAVIGATION_LINKS = [
 
 function App() {
 	const [searchParams, setSearchParams] = useSearchParams({
-		q: "",
+		query: "",
 	});
+
+	const query = searchParams.get("query") as string;
+
+	useEffect(() => {
+		console.log("Log:searchParams", searchParams);
+	}, [searchParams]);
+
 	return (
 		<>
+			<input
+				type="text"
+				value={query}
+				onChange={(e) => {
+					console.log();
+					setSearchParams((prev) => {
+						prev.set("query", e.target.value);
+						return prev;
+					});
+				}}
+			/>
+
 			<ul>
 				{NAVIGATION_LINKS.map((menu) => (
 					<li key={menu.id}>
